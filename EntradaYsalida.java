963f608d9500c5fe628bc6cb311106f889138c07
MohammedChoudna <116067452+MohammedChoudna@users.noreply.github.com>
2022-10-18 10:45:45 +0200

    Add files via upload
    Mohammed CHOUDNA 
    Ejercicio de JAVA

---
+++ EntradaYsalida.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.entradaysalida;
+
+/**
+ *
+ * @author Mohammed
+ */
+public class EntradaYsalida {
+
+    public static void main(String[] args) {
+        int entero = 10 ; 
+        float decimal = 6.2f ; 
+        char caracter = 'A'; 
+        String cadena ="HOLA QUE TAL"; 
+        System.out.println("El numero entero es "+entero);
+        System.out.println("El decimal es "+decimal);
+        System.out.println("El caracter es "+caracter);
+        System.out.println("La cadena es de"+cadena);
+       
+        
+        
+    }
+}
